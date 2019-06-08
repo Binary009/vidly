69e91cc383a541960b5004c3a5a938f0b6ea2c03
Sxmmie <samoski93@gmail.com>
2019-06-08 02:03:26 +0100

    Added model classes Genre and Movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    # class-attr
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rates = models.FloatField()
+    # associate each movie to a genre
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
